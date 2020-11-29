99f58ab0137efca57e236a12244d72fb5ab15097
TheEgid <egid.f1@gmail.com>
2020-11-29 20:06:18 +0300

    Create places app

--- where_to_go/places/apps.py
+++ where_to_go/places/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class WhereToGoConfig(AppConfig):
-    name = 'where_to_go_app'
+    name = 'places'
--- where_to_go/where_to_go/urls.py
+++ where_to_go/where_to_go/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from where_to_go_app import views
+from places import views
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
