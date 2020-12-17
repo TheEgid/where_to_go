bd219902f3eedf82fc7314ed20d5a5d41eaa1c40
TheEgid <egid.f1@gmail.com>
2020-12-17 21:05:49 +0300

    Hide Admin Numerations

--- places/admin.py
+++ places/admin.py
@@ -1,13 +1,14 @@
 from django.contrib.gis import admin
 from django.utils.html import format_html
 from django.contrib.gis.db import models as geomodels
-from adminsortable2.admin import SortableInlineAdminMixin
+from adminsortable2.admin import SortableInlineAdminMixin, SortableAdminMixin
 from .widgets import LatLongWidget
 from .models import Place, Image, PlaceGeo
+from .utils import resize_sides
 
 
 @admin.register(Image)
-class ImageAdmin(admin.ModelAdmin):
+class ImageAdmin(SortableAdminMixin, admin.ModelAdmin):
     model = Image
 
 
@@ -31,24 +32,12 @@ class ImageInline(SortableInlineAdminMixin, admin.TabularInline):
     fields = (('image', ), 'preview_image')
 
     def preview_image(self, obj):
-        width = obj.image.width
-        height = obj.image.height
-        MAX_SIZE = 99
-        if width > height:
-            resized_width = MAX_SIZE
-            resized_height = int(
-                round((MAX_SIZE / float(width)) * height))
+        if obj:
+            width, height = resize_sides(obj.image.width, obj.image.height)
+            return format_html(f'<img src="{obj.image.url}" '
+                               f'width="{width}" height={height} />')
         else:
-            resized_height = MAX_SIZE
-            resized_width = int(
-                round((MAX_SIZE / float(height)) * width))
-        return format_html(
-            '<img src="{url}" width="{width}" height={height} />'.format(
-                url=obj.image.url,
-                width=resized_width,
-                height=resized_height,
-            )
-        )
+            return format_html("No Preview")
 
 
 @admin.register(Place)
